d70908b119eef9b7a6953780e54648ad72e023fd
H. Kamran <hkamran@unisontech.org>
2021-02-16 01:41:02 -0800

    Initial Commit

---
+++ Xenon Reader/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Xenon Reader
+//
+//  Created by H. Kamran on 2/16/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Xenon Reader/Xenon_ReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Xenon_ReaderApp.swift
+//  Xenon Reader
+//
+//  Created by H. Kamran on 2/16/21.
+//
+
+import SwiftUI
+
+@main
+struct Xenon_ReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
